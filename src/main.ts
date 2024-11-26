329d4116821fc3cff3da63014293d11d44300b8e
HEX9CF <hex9cf@aliyun.com>
2024-11-26 11:45:56 +0800

    Import ec and sha256

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+
+import { ec as EC } from 'elliptic';
+import sha256 from 'crypto-js/sha256';
+const ec = new EC('secp256k1');
+export { ec, sha256 }
+
+createApp(App).mount('#app')
+
