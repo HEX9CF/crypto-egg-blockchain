4b069acbb3fa6da480adea5bf51c23d622e91aaf
HEX9CF <hex9cf@aliyun.com>
2024-11-27 11:39:11 +0800

    Optimize block list display

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 import { Chain } from '@/model/chain';
-import { Block } from '@/model/block';
 import { Key } from '@/model/key';
 import { Transaction } from '@/model/transaction';
 
@@ -85,33 +84,38 @@ function clickValidate(): void {
         <span>挖矿奖励：{{ chain.minerReward }}</span><br/>
         <span>区块数量：{{ chain.blocks.length }}</span><br/>
         <span>交易池大小：{{ chain.transactionPool.length }}</span><br/>
-        <h2>区块列表</h2>
-        <p v-for="(block, index) in chain.blocks">
-          <h3>区块{{ index }}</h3>
-          <span>时间戳：{{ block.timestamp }}</span><br/>
-          <span>区块哈希：{{ block.hash }}</span><br/>
-          <span>上一个区块哈希：{{ block.previousHash }}</span><br/>
-          <span>随机数：{{ block.nonce }}</span><br/>
-          <h4>交易列表</h4>
-          <p v-for="(transaction, index) in block.transactions">
-            <span>发送方：{{ transaction.from }}</span><br/>
-            <span>接收方：{{ transaction.to }}</span><br/>
-            <span>金额：{{ transaction.amount }}</span><br/>
-            <span>签名：{{ transaction.signature }}</span><br/>
-            <span>时间戳：{{ transaction.timestamp }}</span>
+        <div v-if="chain.blocks.length > 0">
+          <h2>区块列表</h2>
+          <p v-for="(block, index) in chain.blocks">
+            <h3>区块{{ index }}</h3>
+            <span>时间戳：{{ block.timestamp }}</span><br/>
+            <span>区块哈希：{{ block.hash }}</span><br/>
+            <span>上一个区块哈希：{{ block.previousHash }}</span><br/>
+            <span>随机数：{{ block.nonce }}</span><br/>
+            <div v-if="block.transactions.length > 0">
+              <h4>交易列表</h4>
+              <p v-for="(transaction, index) in block.transactions">
+                <span>发送方：{{ transaction.from }}</span><br/>
+                <span>接收方：{{ transaction.to }}</span><br/>
+                <span>金额：{{ transaction.amount }}</span><br/>
+                <span>签名：{{ transaction.signature }}</span><br/>
+                <span>时间戳：{{ transaction.timestamp }}</span>
+              </p>
+            </div>
           </p>
-        </p>
-        <h2>交易池列表</h2>
-        <p v-for="(transaction, index) in chain.transactionPool">
-          <span>发送方：{{ transaction.from }}</span><br/>
-          <span>接收方：{{ transaction.to }}</span><br/>
-          <span>金额：{{ transaction.amount }}</span><br/>
-          <span>签名：{{ transaction.signature }}</span><br/>
-          <span>时间戳：{{ transaction.timestamp }}</span>
-        </p>
+          <div v-if="chain.transactionPool.length > 0">
+            <h2>交易池列表</h2>
+            <p v-for="(transaction, index) in chain.transactionPool">
+              <span>发送方：{{ transaction.from }}</span><br/>
+              <span>接收方：{{ transaction.to }}</span><br/>
+              <span>金额：{{ transaction.amount }}</span><br/>
+              <span>签名：{{ transaction.signature }}</span><br/>
+              <span>时间戳：{{ transaction.timestamp }}</span>
+            </p>
+          </div>
+        </div>
       </p>
     </div>
-
   </main>
 </template>
 
