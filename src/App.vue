5b18a4354891ec35a77b14e3c82115f582fd8a36
HEX9CF <hex9cf@aliyun.com>
2024-11-29 11:10:55 +0800

    Use element-plus

--- src/App.vue
+++ src/App.vue
@@ -19,6 +19,14 @@ import {
 import { message } from '@/stores/message';
 import { inventory, chicken, donateForm } from '@/stores/farm';
 import { chain, key, transactionForm } from '@/stores/blockchain';
+
+const colors = [
+  { color: '#f56c6c', percentage: 20 },
+  { color: '#e6a23c', percentage: 40 },
+  { color: '#5cb87a', percentage: 60 },
+  { color: '#1989fa', percentage: 80 },
+  { color: '#6f7ad3', percentage: 100 },
+]
 </script>
 
 <template>
@@ -26,112 +34,188 @@ import { chain, key, transactionForm } from '@/stores/blockchain';
   </header>
 
   <main>
-    <div>
-      <h1>虚拟养鸡场</h1>
-      <span>生蛋进度：{{ chicken.progress }}%</span><br/>
-      <span>饲料库存：{{ inventory.food }}</span><br/>
-      <span>加密蛋库存：{{ inventory.egg }}</span><br/><br/>
-      <input type="button" value="领取饲料" @click="clickGetFood()"/>&nbsp;
-      <input type="button" value="喂食" @click="clickFeed()"/>&nbsp;
-      <input type="button" value="收蛋" @click="clickCollectEgg()"/>&nbsp;<br/><br/>
-      <label>捐蛋数量：</label>
-      <input type="text" v-model="donateForm.amount"/>&nbsp;
-      <input type="button" value="捐蛋" @click="clickDonateEgg()"/>&nbsp;
-      <p v-if="message.farm !== ''">
-        <span>{{ message.farm }}</span>
-      </p>
-    </div>
-
-    <hr/>
-
-    <div>
-        <h1>钱包</h1>
-        <label>私钥：</label>
-        <input type="text" v-model="key.privateKey"/><br/>
-        <label>公钥：</label>
-        <input type="text" v-model="key.publicKey"/><br/><br/>
-        <input type="button" value="清空密钥对" @click="clickClearKey()"/>&nbsp;
-        <input type="button" value="生成密钥对" @click="clickGenKey()"/>&nbsp;
-        <input type="button" value="从私钥导入" @click="clickKeyFromPrivate()"/>&nbsp;
-        <input type="button" value="验证密钥对" @click="clickValidateKey()"/>&nbsp;
-        <p v-if="message.key !== ''">
-          <span>{{ message.key }}</span>
-        </p>
-    </div>
-
-    <hr/>
-
-    <div>
-        <h1>交易</h1>
-        <form>
-          <label>收款人公钥：</label>
-          <input type="text" v-model="transactionForm.to"/><br/>
-          <label>转账金额：</label>
-          <input type="text" v-model="transactionForm.amount"/><br/>
-          <label>交易信息：</label>
-          <input type="text" v-model="transactionForm.message"/><br/><br/>
-          <input type="button" value="清空表单" @click="transactionForm.to = ''; transactionForm.amount = 0; transactionForm.message = '';"/>&nbsp;
-          <input type="button" value="添加交易" @click="clickAddTransaction()"/>&nbsp;
-        </form>
-        <p v-if="message.transaction !== ''">
-          <span>{{ message.transaction }}</span>
-        </p>
-    </div>
-
-    <hr/>
-
-    <div>
-        <h1>区块链</h1>
-        <span>难度：{{ chain.difficulty }}</span><br/>
-        <span>挖矿奖励：{{ chain.minerReward }}</span><br/>
-        <span>区块数量：{{ chain.blocks.length }}</span><br/>
-        <span>交易池大小：{{ chain.transactionPool.length }}</span><br/><br/>
-
-        <div>
-          <input type="button" value="挖矿" @click="clickMine()"/>&nbsp;
-          <input type="button" value="验证区块链" @click="clickValidateChain()"/>
-          <p v-if="message.block !== ''">
-            <span>{{ message.block }}</span>
-          </p>
+    <el-card>
+      <template #header>
+        <div class="card-header">
+          <span>虚拟养鸡场</span>
         </div>
+      </template>
+      <el-alert v-if="message.farm !== ''" :title="message.farm" type="info" show-icon></el-alert><br/>
+      <el-progress type="dashboard" :percentage="chicken.progress">
+        <template #default="{ percentage }">
+          <span class="percentage-value">{{ percentage }}%</span>
+          <span class="percentage-label">生蛋进度</span>
+        </template>
+      </el-progress>
+      <el-row>
+        <el-col :span="4">
+          <el-statistic title="饲料库存" :value="inventory.food"></el-statistic>
+        </el-col>
+        <el-col :span="4">
+          <el-statistic title="加密蛋库存" :value="inventory.egg"></el-statistic>
+        </el-col>
+      </el-row>
+      <el-row :gutter="20" style="margin-top: 20px;">
+        <el-col :span="2">
+          <el-button type="primary" @click="clickGetFood">领取饲料</el-button>
+        </el-col>
+        <el-col :span="2">
+          <el-button type="primary" @click="clickFeed">喂食</el-button>
+        </el-col>
+        <el-col :span="2">
+          <el-button type="primary" @click="clickCollectEgg">收蛋</el-button>
+        </el-col>
+      </el-row>
+      <el-row style="margin-top: 20px;">
+        <el-col :span="8">
+          <el-form>
+            <el-form-item label="捐蛋数量">
+              <el-input v-model="donateForm.amount"></el-input>
+            </el-form-item>
+            <el-button type="primary" @click="clickDonateEgg">捐蛋</el-button>
+          </el-form>
+        </el-col>
+      </el-row>
+    </el-card>
 
-        <div v-if="chain.blocks.length > 0">
-          <h2>区块列表</h2>
-          <div v-for="(block, index) in chain.blocks">
-            <h3>区块{{ index }}</h3>
-            <span>时间戳：{{ block.timestamp }} ({{ new Date(block.timestamp).toLocaleString() }})</span><br/>
-            <span>区块哈希：{{ block.hash }}</span><br/>
-            <span>上一个区块哈希：{{ block.previousHash }}</span><br/>
-            <span>随机数：{{ block.nonce }}</span><br/>
-            <div v-if="block.transactions.length > 0">
-              <h4>交易列表</h4>
-              <div v-for="(transaction, index) in block.transactions">
-                <span>时间戳：{{ transaction.timestamp }} ({{ new Date(transaction.timestamp).toLocaleString() }})</span><br/>
-                <span>发送方：{{ transaction.from }}</span><br/>
-                <span>接收方：{{ transaction.to }}</span><br/>
-                <span>金额：{{ transaction.amount }}</span><br/>
-                <span>交易信息：{{ transaction.message }}</span><br/>
-                <span>签名：{{ transaction.signature }}</span><br/><br/>
+    <br/>
+
+    <el-card>
+      <template #header>
+        <div class="card-header">
+          <span>钱包</span>
+        </div>
+      </template>
+      <el-alert v-if="message.key !== ''" :title="message.key" type="info" show-icon></el-alert><br/>
+      <el-form>
+        <el-form-item label="私钥">
+          <el-input v-model="key.privateKey"></el-input>
+        </el-form-item>
+        <el-form-item label="公钥">
+          <el-input v-model="key.publicKey"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="clickClearKey">清空密钥对</el-button>
+          <el-button type="primary" @click="clickGenKey">生成密钥对</el-button>
+          <el-button type="primary" @click="clickKeyFromPrivate">从私钥导入</el-button>
+          <el-button type="primary" @click="clickValidateKey">验证密钥对</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
+
+    <br/>
+
+    <el-card>
+      <template #header>
+        <div class="card-header">
+          <span>交易</span>
+        </div>
+      </template>
+      <el-alert v-if="message.transaction !== ''" :title="message.transaction" type="info" show-icon></el-alert><br/>
+      <el-form>
+        <el-form-item label="收款人公钥">
+          <el-input v-model="transactionForm.to"></el-input>
+        </el-form-item>
+        <el-form-item label="转账金额">
+          <el-input v-model="transactionForm.amount"></el-input>
+        </el-form-item>
+        <el-form-item label="交易信息">
+          <el-input v-model="transactionForm.message"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="transactionForm.to = ''; transactionForm.amount = 0; transactionForm.message = '';">清空表单</el-button>
+          <el-button type="primary" @click="clickAddTransaction">添加交易</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
+
+    <br/>
+
+    <el-card>
+      <template #header>
+        <div class="card-header">
+          <span>区块链</span>
+        </div>
+      </template>
+      <el-alert v-if="message.block !== ''" :title="message.block" type="info" show-icon style="margin-top: 20px;"></el-alert><br/>
+      <el-row>
+        <el-col :span="6">
+          <el-statistic title="难度" :value="chain.difficulty"></el-statistic>
+        </el-col>
+        <el-col :span="6">
+          <el-statistic title="挖矿奖励" :value="chain.minerReward"></el-statistic>
+        </el-col>
+        <el-col :span="6">
+          <el-statistic title="区块数量" :value="chain.blocks.length"></el-statistic>
+        </el-col>
+        <el-col :span="6">
+          <el-statistic title="交易池大小" :value="chain.transactionPool.length"></el-statistic>
+        </el-col>
+      </el-row>
+      <el-row :gutter="20" style="margin-top: 20px;">
+        <el-col :span="2">
+          <el-button type="primary" @click="clickValidateChain">验证区块链</el-button>
+        </el-col>
+        <el-col :span="2">
+          <el-button type="primary" @click="clickMine">挖矿</el-button>
+        </el-col>
+      </el-row>
+      <div v-if="chain.blocks.length > 0" style="margin-top: 20px;">
+        <h2>区块列表</h2>
+        <el-timeline>
+          <el-timeline-item v-for="(block, index) in chain.blocks" :timestamp="new Date(block.timestamp).toLocaleString()" placement="top">
+            <el-card>
+              <h3>区块{{ index + 1 }}</h3>
+              <p>
+                <span>时间戳: {{ block.timestamp }} ({{ new Date(block.timestamp).toLocaleString() }})</span><br/>
+                <span>区块哈希: {{ block.hash }}</span><br/>
+                <span>上一个区块哈希: {{ block.previousHash }}</span><br/>
+                <span>随机数: {{ block.nonce }}</span>
+              </p>
+              <div v-if="block.transactions.length > 0">
+                <hr/>
+                <h4>交易列表</h4>
+                <p v-for="(transaction, index) in block.transactions" :key="index">
+                  <h5>交易{{ index + 1 }}</h5>
+                  <span>时间戳: {{ transaction.timestamp }} ({{ new Date(transaction.timestamp).toLocaleString() }})</span><br/>
+                  <span>发送方: {{ transaction.from }}</span><br/>
+                  <span>接收方: {{ transaction.to }}</span><br/>
+                  <span>金额: {{ transaction.amount }}</span><br/>
+                  <span>交易信息: {{ transaction.message }}</span><br/>
+                  <span>签名: {{ transaction.signature }}</span>
+                </p>
               </div>
-            </div>
-            <br/>
-          </div>
-          <div v-if="chain.transactionPool.length > 0">
-            <h2>交易池列表</h2>
-            <div v-for="(transaction, index) in chain.transactionPool">
-              <span>时间戳：{{ transaction.timestamp }} ({{ new Date(transaction.timestamp).toLocaleString() }})</span><br/>
-              <span>发送方：{{ transaction.from }}</span><br/>
-              <span>接收方：{{ transaction.to }}</span><br/>
-              <span>金额：{{ transaction.amount }}</span><br/>
-              <span>交易信息：{{ transaction.message }}</span><br/>
-              <span>签名：{{ transaction.signature }}</span><br/><br/>
-            </div>
-          </div>
+            </el-card>
+          </el-timeline-item>
+        </el-timeline>
+        <div v-if="chain.transactionPool.length > 0">
+          <h2>交易池列表</h2>
+          <el-card v-for="(transaction, index) in chain.transactionPool" :key="index" style="margin-bottom: 10px;">
+            <h3>交易{{ index + 1 }}</h3>
+            <p>
+              <span>时间戳: {{ transaction.timestamp }} ({{ new Date(transaction.timestamp).toLocaleString() }})</span><br/>
+              <span>发送方: {{ transaction.from }}</span><br/>
+              <span>接收方: {{ transaction.to }}</span><br/>
+              <span>金额: {{ transaction.amount }}</span><br/>
+              <span>交易信息: {{ transaction.message }}</span><br/>
+              <span>签名: {{ transaction.signature }}</span>
+            </p>
+          </el-card>
         </div>
-    </div>
+      </div>
+    </el-card>
   </main>
 </template>
 
 <style scoped>
-
+.percentage-value {
+  display: block;
+  margin-top: 10px;
+  font-size: 28px;
+}
+.percentage-label {
+  display: block;
+  margin-top: 10px;
+  font-size: 12px;
+}
 </style>
